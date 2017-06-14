103e4673c05c80d26de307013f50108a52783452
augustocbx <augustocbx@gmail.com>
2017-06-14 18:36:12 -0300

    Learning RN, cool!!!

--- index.ios.js
+++ index.ios.js
@@ -3,8 +3,20 @@
 
 // Import a libray to help crate a Component
 import React from 'react';
-
+import {
+  AppRegistry,
+  View,
+} from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/album_list';
 
 // Create a Component
+const App = () => (
+  <View>
+    <Header headerText="Albums" />
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
